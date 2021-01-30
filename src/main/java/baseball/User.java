c32adf5ca225e13f63d53e47c303cdb34b26cac9
BohGyu <qhrb1997@naver.com>
2021-01-30 22:41:34 +0900

    feat(User.java) : play event
    This method prints the process of playing once
    for one random number.

--- src/main/java/baseball/User.java
+++ src/main/java/baseball/User.java
@@ -1,2 +1,23 @@
-package baseball;public class User {
+package baseball;
+import java.util.Scanner;
+
+public class User {
+    Scanner scanner = new Scanner(System.in);
+
+    public void play(Computer computer, int randomNum) {
+        while (true) {
+            computer.printPlaying();
+            int receivedNum = computer.filterReceivedNum(scanner.nextInt());
+
+            String randomNumStr = Integer.toString(randomNum);
+            String receivedNumStr = Integer.toString(receivedNum);
+
+            computer.printCount(randomNumStr, receivedNumStr);
+
+            if (computer.getStrikeCount(randomNumStr, receivedNumStr) == 3) {
+                computer.printEndGame();
+                break;
+            }
+        }
+    }
 }
