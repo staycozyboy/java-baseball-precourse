9a9bb4289a5ae6df0edff8fd6ff0213aa79a26fb
BohGyu <qhrb1997@naver.com>
2021-01-30 22:42:47 +0900

    feat(Application.java) : program

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -5,6 +5,18 @@ import java.util.Scanner;
 public class Application {
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
-        // TODO 구현 진행
+
+        Computer computer = new Computer();
+        User user = new User();
+
+        while(true) {       //한 번의 게임
+            int random = computer.createRandomNum();
+            user.play(computer, random);
+
+            int restartOrNotNum = scanner.nextInt();
+            if(computer.restartOrNot(restartOrNotNum) == false) {   //2일 경우
+                break;
+            }
+        }
     }
 }
