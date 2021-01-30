e0cd253e956836168b95cddd00a0d6e0592547d6
wansoo05 <yk3626@naver.com>
2021-01-30 14:38:02 +0900

    fix: Computer클래스에서 랜덤숫자 3개 생성구현 수정

--- src/main/java/baseball/Computer.java
+++ src/main/java/baseball/Computer.java
@@ -1,20 +1,25 @@
 package baseball;
 
+
 import utils.RandomUtils;
 
 public class Computer{
     private static final int NUMBER_LIMIT = 3;
     private static final int START = 1;
     private static final int END = 9;
-
-    public static String generateRandomNumbers() {
-        String number = "";
+    private String number = "";
+    public Computer() {
+        generateNumber(number);
+    }
+    public void generateNumber(String number) {
         while(number.length() < NUMBER_LIMIT) {
             int randomNumber = RandomUtils.nextInt(START, END);
             if (!number.contains(String.valueOf(randomNumber))) {
                 number = number + randomNumber;
             }
         }
+    }
+    public String getNumber() {
         return number;
     }
 }
