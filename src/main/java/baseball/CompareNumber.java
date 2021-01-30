03ce1e00cce3a096648f9cb69435484b559d02f1
wansoo05 <yk3626@naver.com>
2021-01-31 02:14:11 +0900

    feat: 결과 판단및 흰트제공 구현

--- src/main/java/baseball/CompareNumber.java
+++ src/main/java/baseball/CompareNumber.java
@@ -1,6 +1,21 @@
 package baseball;
 
 public class CompareNumber {
+    public static String resultMessage(String computerNumber, String playerNumber) {
+        int strike = countStrike(computerNumber, playerNumber);
+        int ball = countBall(computerNumber, playerNumber);
+
+        if (strike == 0 && ball == 0) {
+            return "낫싱";
+        }
+        if (strike == 0) {
+            return ball + "볼";
+        }
+        if (ball == 0) {
+            return strike + "스트라이크";
+        }
+        return ball + "볼" + strike + "스트라이크";
+    }
     private static int countStrike(String computerNumber, String playerNumber) {
         int strike = 0;
         for (int i = 0; i < computerNumber.length(); i++) {
--- src/main/java/baseball/GamePlay.java
+++ src/main/java/baseball/GamePlay.java
@@ -10,6 +10,28 @@ public class GamePlay {
         player = new Player(scanner);
     }
     public void playGame(){
+        while (true) {
 
+            System.out.print("숫자를 입력해주세요 : ");
+            try {
+                number = player.createNumber();
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+                continue;
+            }
+            if (isGameOver(computer.getNumber(), number)) {
+                break;
+            }
+            System.out.println(CompareNumber.resultMessage(computer.getNumber(), number));
+        }
+    }
+
+    public boolean isGameOver(String computerNumber, String playerNumber) {
+        if (computerNumber.equals(playerNumber)) {
+            System.out.println("3스트라이크");
+            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+            return true;
+        }
+        return false;
     }
 }
