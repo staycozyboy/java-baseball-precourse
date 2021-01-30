571dd2e2b7ad24f7682c672828840c9478bcafbd
wansoo05 <yk3626@naver.com>
2021-01-31 01:16:21 +0900

    feat: 예외메세지 추가

--- src/main/java/baseball/Player.java
+++ src/main/java/baseball/Player.java
@@ -26,18 +26,18 @@ public class Player{
     public void isRightValue(String number) {
         for (int i = 0; i < NUMBER_LIMIT; i++) {
             if ((number.charAt(i) - 48) < START || (number.charAt(i) - 48) > END) {
-                throw new IllegalArgumentException();
+                throw new IllegalArgumentException("숫자만 입력 가능합니다.");
             }
         }
     }
     public void isDuplicated(String number) {
         for (int i = 0; i < NUMBER_LIMIT - 1; i++) {
             if (number.charAt(i) == number.charAt(i+1)) {
-                throw new IllegalArgumentException();
+                throw new IllegalArgumentException("중복된 숫자가 있습니다.");
             }
         }// 1번째 - 2번째, 2번째 - 3번째 같은지 비교
         if(number.charAt(0) == number.charAt(2)) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("중복된 숫자가 있습니다.");
         }// 1번째 - 3번째 같은지 비교
     }
 }
