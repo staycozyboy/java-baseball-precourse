5f3334cf1ba16c87f9703ac53507ee1f0ee699f0
BohGyu <qhrb1997@naver.com>
2021-01-30 22:26:25 +0900

    feat(Computer.java) : printCount, printPlaying, printEndGame event related output functions

--- src/main/java/baseball/Computer.java
+++ src/main/java/baseball/Computer.java
@@ -23,4 +23,25 @@ public class Computer {
 
         return strikeCount;
     }
+
+    public void printCount(String randomStr, String receivedNum) {       //출력
+        if (getStrikeCount(randomStr, receivedNum) == 0 && getBallCount(randomStr, receivedNum) == 0) {   //둘 다 0일 경우
+            System.out.println("낫싱");
+        } else if (getStrikeCount(randomStr, receivedNum) != 0 && getBallCount(randomStr, receivedNum) == 0) {    //볼카운트만 0일 경우
+            System.out.printf("%d스트라이크 \n", getStrikeCount(randomStr, receivedNum));
+        } else if (getStrikeCount(randomStr, receivedNum) == 0 && getBallCount(randomStr, receivedNum) != 0) {    //스트라잌카운트만 0일 경우
+            System.out.printf("%d볼 \n", getBallCount(randomStr, receivedNum));
+        } else  {    //둘다 0이 아닐 경우
+            System.out.printf("%d볼 %d스트라이크 \n", getBallCount(randomStr, receivedNum), getStrikeCount(randomStr, receivedNum));
+        }
+    }
+
+    public void printPlaying() {
+        System.out.print("숫자를 입력하세요 : ");
+    }
+
+    public void printEndGame() {
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요");
+    }
 }
