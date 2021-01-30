b5fc7b475ec3b8953590612a06f33d13fb909df7
staycozyboy <dkc984@likelion.org>
2021-01-31 01:40:26 +0900

    fix : OutputHandler의 매직넘버 상수화

--- src/main/java/baseball/io/OutputHandler.java
+++ src/main/java/baseball/io/OutputHandler.java
@@ -1,27 +1,30 @@
 package baseball.io;
 
 public class OutputHandler {
+    private static final int NONE = 0;
+    private static final int OUT = 3;
+
     private OutputHandler() {
     }
 
     public static void printResult(int strike, int ball) {
         StringBuilder sb = new StringBuilder();
 
-        if(strike == 0 && ball == 0) {
+        if(strike == NONE && ball == NONE) {
             System.out.println("낫싱");
             return;
         }
 
-        if(ball > 0) {
+        if(ball > NONE) {
             sb.append(String.format("%d 볼 ", ball));
         }
 
-        if(strike > 0) {
+        if(strike > NONE) {
             sb.append(String.format("%d 스트라이크", strike));
         }
 
         System.out.println(sb.toString());
-        if(strike == 3) {
+        if(strike == OUT) {
             System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
         }
     }
