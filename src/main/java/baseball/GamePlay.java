ab075554da0a628f13a52317d145df31dbecff9e
wansoo05 <yk3626@naver.com>
2021-01-31 03:27:21 +0900

    feat: 재시작 묻는 기능 구현

--- src/main/java/baseball/GamePlay.java
+++ src/main/java/baseball/GamePlay.java
@@ -11,6 +11,7 @@ public class GamePlay {
         this.scanner = scanner;
     }
     public void playGame(){
+        String isRestart;
         while (true) {
             System.out.print("숫자를 입력해주세요 : ");
             try {
@@ -25,14 +26,38 @@ public class GamePlay {
             }
             System.out.println(CompareNumber.resultMessage(computer.getNumber(), number));
         }
+        isRestart = replayGame();
+        if(isRestart.equals("1")) {
+            playGame();
+        }
+        System.out.println("종료");
     }
 
-    public boolean isGameOver(String computerNumber, String playerNumber) {
+    private boolean isGameOver(String computerNumber, String playerNumber) {
         if (computerNumber.equals(playerNumber)) {
             System.out.println("3스트라이크");
             System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
             return true;
         }
         return false;
+    }
+    private String replayGame(){
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        String input = scanner.nextLine();
+        try {
+            isRightInput(input);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            replayGame();
+        }
+        return input;
+    }
+    private void isRightInput(String input) throws IllegalArgumentException {
+        if (!isValidInput(input)) {
+            throw new IllegalArgumentException("잘못된 입력값입니다.");
+        }
+    }
+    private boolean isValidInput(String input){
+        return input.equals("1") || input.equals("2");
     }
 }
