0ca18ef4a2eaa04a965382d5d544e80c3b5dd3f5
BohGyu <qhrb1997@naver.com>
2021-02-02 18:03:32 +0900

    feat : 재시작과 관련된 제어기능 구현

--- src/main/java/baseball/Controller.java
+++ src/main/java/baseball/Controller.java
@@ -1,2 +1,45 @@
-package baseball;public class Controller {
+package baseball;
+
+import java.util.Scanner;
+
+public class Controller {
+    private static int START = 1;
+    private static int END = 2;
+    private static int MAX_COUNT = 3;
+    public static boolean GAMEOVER = false;     //캡슐화깨고 전역으로 게임종료에 대한 정보 받는다.
+
+
+    public static void restartOrNot() {
+        Scanner scanner = new Scanner(System.in);
+        OutputHandler.printEnd();
+        int n = scanner.nextInt();
+
+        try {
+            InputHandler.isVaildRestartNum(n);
+        } catch (IllegalArgumentException e) {
+            e.printStackTrace();
+        }
+
+        if(n == START) {
+            GameLogic.run();
+        } else if(n == END) {
+            Controller.stop();
+        } else {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public static boolean isThreeStrike(int random, int receivedNum) {
+        Computer computer = Computer.getInstance();
+
+        if (computer.getStrikeCount(random, receivedNum) == MAX_COUNT){
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static void stop() {
+        GAMEOVER = true;
+    }
 }
