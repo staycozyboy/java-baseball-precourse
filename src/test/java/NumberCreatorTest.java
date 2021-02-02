e6ecefa0d4f838013ee9bcaa917947df35a019e0
BohGyu <qhrb1997@naver.com>
2021-02-03 01:09:21 +0900

    test : 3자리 각기 다른 난수 생성에 관한 테스트 코드

--- src/test/java/NumberCreatorTest.java
+++ src/test/java/NumberCreatorTest.java
@@ -1,2 +1,29 @@
-package PACKAGE_NAME;public class NumberCreatorTest {
+import baseball.NumberCreator;
+import org.junit.jupiter.api.Test;
+import utils.RandomUtils;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class NumberCreatorTest {
+    public boolean isNonDuplicateRandomNum(int num) {
+        if (num / 100 != num / 10 % 10 && num / 100 != num % 10) {
+            if (num / 10 % 10 != num % 10) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Test
+    public void 세자리_숫자가_각기_다른수로_만들어졌는지() {
+        NumberCreator numberCreator = new NumberCreator();
+
+        int random1 = numberCreator.createNonDuplicateRandomNum();
+        int random2 = numberCreator.createNonDuplicateRandomNum();
+        int random3 = numberCreator.createNonDuplicateRandomNum();
+
+        assertThat(isNonDuplicateRandomNum(random1));
+        assertThat(isNonDuplicateRandomNum(random2));
+        assertThat(isNonDuplicateRandomNum(random3));
+    }
 }
