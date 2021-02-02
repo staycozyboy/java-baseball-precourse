13ca8953dd9e6e8f2be5dbd3bca030667d668eb6
BohGyu <qhrb1997@naver.com>
2021-02-02 18:08:30 +0900

    feat : 3개의 임의의 수로 난수 만드는 일급 컬렉션 구현

--- src/main/java/baseball/NumberCreator.java
+++ src/main/java/baseball/NumberCreator.java
@@ -1,2 +1,36 @@
-package baseball;public class NumberCreator {
+package baseball;
+import utils.RandomUtils;
+
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
+public class NumberCreator {
+    public Set<Integer> randomNumbers;
+
+    public boolean isValidSize(Set<Integer> randomNumbers) {
+        if (randomNumbers.size() == 3) {
+            return true;
+        }
+        return false;
+    }
+
+    public void createValidSet() {
+        randomNumbers = new HashSet<>();
+        while (!isValidSize(randomNumbers)) {
+            randomNumbers.add(RandomUtils.nextInt(1, 9));
+        }
+    }
+
+    public int createNonDuplicateRandomNum() {         //서로 다른 수를 지닌 임의의 3자릿수 만들기.
+        createValidSet();
+        Iterator<Integer> iterator = randomNumbers.iterator();
+        int randomNum = 0;
+        int digit = 1;
+        while (iterator.hasNext()) {
+            randomNum += iterator.next() * digit;
+            digit *= 10;
+        }
+        return randomNum;
+    }
 }
