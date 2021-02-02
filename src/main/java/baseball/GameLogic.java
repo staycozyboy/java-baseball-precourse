89d2e2521a6d5485bc47c011e29e38baa9810c1b
BohGyu <qhrb1997@naver.com>
2021-02-02 18:05:09 +0900

    feat : 게임 돌아가는 구조 구현

--- src/main/java/baseball/GameLogic.java
+++ src/main/java/baseball/GameLogic.java
@@ -1,2 +1,16 @@
-package baseball;public class GameLogic {
+package baseball;
+
+public class GameLogic {
+    public static void run() {
+        Controller.GAMEOVER = false;
+        do {
+            NumberCreator numberCreator = new NumberCreator();
+            int random = numberCreator.createNonDuplicateRandomNum();
+
+            User user = new User();
+            user.play(random);
+
+            Controller.restartOrNot();
+        } while (!Controller.GAMEOVER);
+    }
 }
