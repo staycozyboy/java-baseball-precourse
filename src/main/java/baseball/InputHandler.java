0b3e5db44ed8b8580e9af71768ddbbffe8ccdf45
BohGyu <qhrb1997@naver.com>
2021-02-02 18:07:09 +0900

    feat : 사용자 입력이 유효한지 확인하는 기능

--- src/main/java/baseball/InputHandler.java
+++ src/main/java/baseball/InputHandler.java
@@ -1,2 +1,20 @@
-package baseball;public class InputHandler {
+package baseball;
+
+public class InputHandler {
+    public static void isVaildReceivedNum(int receivedNum) {
+        try {
+            if(receivedNum < 100 || receivedNum > 999) {
+                throw new IllegalArgumentException();
+            }
+        } catch (IllegalArgumentException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public static void isVaildRestartNum(int restartOrNot) {
+        if(restartOrNot != 1 && restartOrNot != 2) {
+            throw new IllegalArgumentException();
+        }
+    }
 }
