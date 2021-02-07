91c725664c3d26f550a21d52b754536be72ef24f
wansoo05 <yk3626@naver.com>
2021-02-07 18:26:57 +0900

    feat: inputNumber가 문자열 받았을때 try catch 추가

--- src/main/java/baseball/Input.java
+++ src/main/java/baseball/Input.java
@@ -2,6 +2,7 @@ package baseball;
 
 import utils.ValidationUtils;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Input {
@@ -24,9 +25,12 @@ public class Input {
         int inputNumber;
         do {
             System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
-            inputNumber = scanner.nextInt();
+            try {
+                inputNumber = scanner.nextInt();
+            } catch (InputMismatchException e) {
+            }
+            scanner.nextLine(); //버퍼비우기
         } while (!ValidationUtils.isValidateNumber(inputNumber));
-        scanner.nextLine(); //버퍼비우기
 
         return inputNumber;
     }
--- src/test/java/baseball/InputTest.java
+++
@@ -1,7 +0,0 @@
-package baseball;
-
-import static org.assertj.core.api.Assertions.*;
-import org.junit.jupiter.api.Test;
-
-public class InputTest {
-}
